1d42f54d387e9855f71b42bbc3a08e0a4ebf1819
MadalinaDinga <madadinga@gmail.com>
2018-01-14 22:47:25 +0200

    + added android project connected to backend

---
+++ parking-android/app/src/main/java/com/android/app/parkinglots/Network.java
@@ -0,0 +1,9 @@
+package com.android.app.parkinglots;
+
+/**
+ * Created by dacianmujdar on 1/10/18.
+ */
+public class Network {
+
+    public static final String URL = "https://parking-django.herokuapp.com/";
+}
